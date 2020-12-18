f4574bf123dc6dc6a71668bb72dd549e9dda14f0
Emily Nagai <emnagai89@gmail.com>
2020-12-18 13:49:04 -0800

    Added propTypes to TimelineEvent

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
+import PropTypes from 'prop-types';
 
 const TimelineEvent = (props) => {  
   return (
@@ -9,7 +10,13 @@ const TimelineEvent = (props) => {
         <li>status: {props.status}</li>
         <li>timeStamp: <Timestamp time={props.timeStamp} /></li>
     </ul>
-  );
-}
+  )
+
+  TimelineEvent.propTypes = {
+    person: PropTypes.string.isRequired,
+    status: PropTypes.string,
+    timeStamp: PropTypes.instanceOf(Timestamp)
+  }
+};
 
 export default TimelineEvent;
