012e95f09faf36b86eb7d17b88a8e81693b70426
Emily Nagai <emnagai89@gmail.com>
2020-12-17 20:14:45 -0800

    Wave 1: TimelineEvent component implemented. Wave 1 tests pass.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
+
+// const timeData = [
+//   {
+//     person: 'Emily',
+//     status: 'Blah Blah',
+//     timeStamp: '3 days ago',
+//   }
+// ]
 
 function App() {
   console.log(timelineData);
@@ -14,6 +23,11 @@ function App() {
         <h1 className="App-title">Application title</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent 
+          person="Emily" 
+          status="Blah" 
+        />
+        {/* <Timestamp /> */}
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+const TimelineEvent = (props) => {  
+  console.log(props)
+  return (
+    <ul className="TimelineEvent">
+        <li>person: {props.person}</li>
+        <li>status: {props.status}</li>
+        <li>timeStamp: <Timestamp time={props.timeStamp} /></li>
+    </ul>
+  );
 }
 
 export default TimelineEvent;
