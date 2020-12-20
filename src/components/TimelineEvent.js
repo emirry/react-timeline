25cd8e0ff6839cfc6443dc7de5c179d79e6fe1b4
emirry <55813574+emirry@users.noreply.github.com>
2020-12-20 14:30:11 -0800

    Update src/components/TimelineEvent.js
    Co-authored-by: Chris M <chris@adadevelopersacademy.org>

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types';
 
 const TimelineEvent = (props) => {  
   return (
-    <ul className="TimelineEvent">
-        <li>person: {props.person}</li>
-        <li>status: {props.status}</li>
-        <li>timeStamp: <Timestamp time={props.timeStamp} /></li>
-    </ul>
+    <article className="timeline-event">
+        <p className="event-person">person: {props.person}</p>
+        <p className="event-status">status: {props.status}</p>
+        <p className='event-time'>timeStamp: <Timestamp time={props.timeStamp} /></p>
+    </article>
   )
   }
   TimelineEvent.propTypes = {
